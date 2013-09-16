8e9a6dbe928712e3df5b0225c4eb6bdaee8003e3
Michael Still <mikal@stillhq.com>
2013-09-16 19:04:56 +1000

    New error meme

--- rotatelogos.py
+++ rotatelogos.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
         new_logo = possible[0]
     else:
         new_logo = '[[File:Trainhassailed.png]]'
+        # new_logo = [[File:Meme harder.png]]
 
     w.post_page('Possible future wiki logos', '\n'.join(possible[1:]))
     w.post_page('Former wiki logos', '\n'.join(archive))
