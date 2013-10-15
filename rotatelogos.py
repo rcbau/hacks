a8a571936032cdc83054195d66c45bd1fa6121b2
Michael Still <mikal@stillhq.com>
2013-10-15 04:50:38 +0000

    Rotate failure meme

--- rotatelogos.py
+++ rotatelogos.py
@@ -23,11 +23,11 @@ if __name__ == '__main__':
     archive = w.get_page('Former wiki logos').split('\n')
     archive.append(main_page[0])
 
-    if possible:
+    if len(possible) > 0:
         new_logo = possible[0]
     else:
-        new_logo = '[[File:Trainhassailed.png]]'
-        # new_logo = [[File:Meme harder.png]]
+        #new_logo = '[[File:Trainhassailed.png]]'
+        new_logo = [[File:Meme harder.png]]
 
     w.post_page('Possible future wiki logos', '\n'.join(possible[1:]))
     w.post_page('Former wiki logos', '\n'.join(archive))
