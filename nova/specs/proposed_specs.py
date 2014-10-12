8a8513394b2af9c9450ce5dc15dd8410d126b707
Michael Still <mikal@stillhq.com>
2014-10-13 09:06:00 +1100

    Start tracking kilo.

--- nova/specs/proposed_specs.py
+++ nova/specs/proposed_specs.py
@@ -21,6 +21,9 @@ def runcmd(cmd):
 if __name__ == '__main__':
     juno_spec_re = re.compile('.*specs/juno/.*\.rst.*')
 
+    kilo_spec_re = re.compile('.*specs/kilo/.*\.rst.*')
+    kilo_impl_re = re.compile('.*specs/kilo/implemented/.*\.rst.*')
+
     
     for line in runcmd('ssh review.openstack.org gerrit query '
                        '--format=json --current-patch-set '
@@ -34,18 +37,29 @@ if __name__ == '__main__':
         if j.get('status') == 'MERGED':
             continue
 
-        spec_match = False
+        spec_match = []
+        implemented = False
         diff = runcmd('git fetch '
                       'https://mikalstill@review.openstack.org/openstack/'
                       'nova-specs '
                       '%s && git format-patch -1 --stdout FETCH_HEAD'
                       % j['currentPatchSet']['ref'])
         for line in diff.split('\n'):
+            m = kilo_impl_re.match(line)
+            if m:
+                implemented = True
+            
             m = juno_spec_re.match(line)
             if m:
-                spec_match = True
-                break
+                spec_match.append('juno')
+            
+            m = kilo_spec_re.match(line)
+            if m:
+                spec_match.append('kilo')
 
-        if spec_match:
+        if implemented:
+            continue
+
+        if 'kilo' in spec_match:
             with open('/tmp/patches/%s' % j['number'], 'w') as f:
                 f.write(diff)
